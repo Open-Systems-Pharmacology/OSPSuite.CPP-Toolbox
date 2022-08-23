fe0021ef6e648e7f5cecbbc430e24a464e1e0384
Yuri05 <Yuri05@github.com>
2022-08-23 12:22:01 +0200

    1. SuiteSparse

--- buildWindows.bat
+++ buildWindows.bat
@@ -1,5 +1,7 @@
 @echo off
 
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+
 mkdir BuildSuiteSparse
 mkdir BuildSuiteSparse\include
 mkdir BuildSuiteSparse\lib64
